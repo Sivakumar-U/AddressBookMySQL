b474885d7283e74307f1fd42a5ac4cf595030a48
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-17 21:03:14 +0530

    Added: To create table for address book

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,1 +1,13 @@
 create schema addressBook;
+use addressBook;
+
+CREATE TABLE addressBook(
+    FirstName VARCHAR(50) NOT NULL PRIMARY KEY,
+    LastName VARCHAR(50) NOT NULL,
+    Address VARCHAR(100) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    State VARCHAR(50) NOT NULL,
+    Zip BIGINT(20) NOT NULL,
+    PhoneNumber BIGINT(20) NOT NULL,
+    Email VARCHAR(40) NOT NULL
+);
